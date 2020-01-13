557b6acbfa574a1d2ab5cc90addd1b32eefec42a
adheshgarg@gmail <adheshgarg@gmail>
2020-01-13 17:27:59 +0530

    first Commit

---
+++ src/main/java/com/example/demoScope/DemoScopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoScope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoScope/DemoScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoScope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
