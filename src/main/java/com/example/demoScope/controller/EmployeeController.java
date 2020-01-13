9bda009c765692e41f91b05091d1574d13b0245b
fructosepadre <utkarshsingh3004@gmail.com>
2020-01-13 21:12:12 +0530

    consumer post and mongo

--- src/main/java/com/example/demoScope/controller/EmployeeController.java
+++ src/main/java/com/example/demoScope/controller/EmployeeController.java
@@ -1,4 +1,11 @@
 package com.example.demoScope.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@GetMapping("/employee")
 public class EmployeeController {
+
 }
