94849b1ca3b2817354f9070f86d3cdc95b21b62f
Nishi <mehtanishii57@gmail.com>
2020-01-13 19:39:19 +0530

    xmladded

--- src/main/java/com/example/demoScope/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/demoScope/service/impl/EmployeeServiceImpl.java
@@ -6,7 +6,14 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,6 +24,59 @@ public class EmployeeServiceImpl implements EmployeeServices {
     @Autowired
     EmployeeRepository employeeRepository;
 
+
+
+
+    // XML starts here----------------------------------------------------------------
+
+
+
+    public static ArrayList<Employee> employeeArray =new ArrayList<Employee>();
+
+    public ArrayList<Employee> readXml()
+    {
+​
+        try
+        {
+            File file = new File("employee.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+            NodeList nodeList = doc.getElementsByTagName("employee");
+            int arraylen = nodeList.getLength();
+​
+            for (int itr = 0; itr < nodeList.getLength(); itr++)
+            {
+                Node node = nodeList.item(itr);
+                if (node.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Employee employee=new Employee();
+                    Element eElement = (Element) node;
+                    firstName=eElement.getElementsByTagName("firstName").item(0).getTextContent();
+                    employee.setFirstName(firstName);
+                    lastName=eElement.getElementsByTagName("lastName").item(0).getTextContent();
+                    employee.setLastName(lastName);
+                    String q=eElement.getElementsByTagName("dateOfBirth").item(0).getTextContent();
+                    dateOfBirth=new SimpleDateFormat("dd/MM/yyyy").parse(q);
+                    employee.setDateOfBirth(dateOfBirth);
+                    experience=Integer.parseInt(eElement.getElementsByTagName("experience").item(0).getTextContent());
+                    employee.setExperience(experience);
+                    employeeArray.add(employee);
+                }
+​
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return employeeArray;
+    }
+
+
+    //// XML ENDS----------------------------------------------------------------------
 
     /// JSON READ STARTS --------------------------------------------------------------
     public ArrayList<Employee> readJSON() throws Exception {
